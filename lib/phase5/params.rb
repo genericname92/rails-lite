af48b922ac4f711016983a0422469876d021d950
App Academy Student <student@appacademy.io>
2015-05-20 14:10:41 -0700

    finishes base rails lite

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -1,4 +1,5 @@
 require 'uri'
+require 'byebug'
 
 module Phase5
   class Params
@@ -12,10 +13,11 @@ module Phase5
     def initialize(req, route_params = {})
       @params = route_params
       parse_www_encoded_form(req.query_string)
+      parse_www_encoded_form(req.body)
     end
 
     def [](key)
-      @params[key]
+      @params[key.to_s]
     end
 
     def to_s
@@ -28,23 +30,40 @@ module Phase5
     # this should return deeply nested hash
     # argument format
     # user[address][street]=main&user[address][zip]=89436
+    #[["user[address[street]", "main"], ["user[address[zip]]", 810384]]
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
-      hash = parse_nested_query(www_encoded_form)
-      @params = @params.merge(hash)
-    end
-
-    def recursion_levels(level)
-      return level if level.length >= 1
-      current = level.shift
-      @params[current] = recursion_levels(level)
+      return if www_encoded_form.nil?
+      arguments = URI::decode_www_form(www_encoded_form)
+      arguments.each do |pair|
+        pair[0] = parse_key(pair[0])
+      end
+      @params = build_params(arguments)
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
       key.split(/\]\[|\[|\]/)
+    end
+
+    def build_params(data)
+      params = {}
+      data.each do |pair|
+        keys = pair.first
+        val = pair.last
+        current = params
+        keys.each_with_index do |key, idx|
+          if idx == keys.length-1
+            current[key] = val
+          else
+            current[key] ||= {}
+            current = current[key]
+          end
+        end
+      end
+      params
     end
   end
 end
